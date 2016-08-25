c1c62180861c562e835f90ff10eed677ed59c48b
Martin Harari Thuresson <martin@46elks.com>
2016-08-25 11:29:33 +0200

    added post to jquery

---
+++ js/traintimes.js
@@ -0,0 +1,16 @@
+
+    var trainquery = '<REQUEST><LOGIN authenticationkey="bcddeeb5166d403ea8e6d78ccf7fdbb9" /><QUERY objecttype="TrainAnnouncement"><FILTER><EQ name="AdvertisedTrainIdent" value="7000" /><EQ name="ActivityType" value="Avgång"/></FILTER><INCLUDE>LocationSignature</INCLUDE><INCLUDE>TimeAtLocation</INCLUDE></QUERY></REQUEST>';
+     $.ajax({
+           url: "http://api.trafikinfo.trafikverket.se/v1.1/data.json", 
+           processData: false,
+           type: "POST",  // type should be POST
+           data: trainquery, // send the string directly
+           contentType:"text/xml; charset=utf-8",
+           dataType:"json",
+           success: function(response){
+             alert(response);
+           },
+           error: function(response) {
+              alert(response);
+           }
+        });
